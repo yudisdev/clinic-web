e49bacc160181ca8c31e3e252aff4f3c72e0fa32
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-09-15 18:40:10 +0800

    update contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { FaMapMarkerAlt } from 'react-icons/fa'
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaClock } from 'react-icons/fa'
 
 const Contact = () => {
     const contactItems = [
         {
             icon: < FaMapMarkerAlt className='text-pink-600 text-xl'/>,
-            title: 'Our Location',
+            title: 'Lokasi',
             content: (
                 <p className='text-gray-600'>
                   Perumahan taman buana permai, block C no 9. <br />
@@ -14,12 +14,29 @@ const Contact = () => {
             )      
         },
         {
-            icon: < FaMapMarkerAlt className='text-pink-600 text-xl'/>,
-            title: 'Our Location',
+            icon: < FaPhoneAlt className='text-pink-600 text-xl'/>,
+            title: 'Nomer Telefon',
             content: (
                 <p className='text-gray-600'>
-                  Perumahan taman buana permai, block C no 9. <br />
-                  Denpasar, Bali.      
+                  Appointments: <a href="#" className='hover:text-pink-600'>+62 812-3892-8001</a>    
+                </p>
+            )      
+        },
+        {
+            icon: < FaEnvelope className='text-pink-600 text-xl'/>,
+            title: 'Email',
+            content: (
+                <p className='text-gray-600'>
+                  Appointments: <a href="#" className='hover:text-pink-600'>Dindabeautycare@gmail.com</a>    
+                </p>
+            )      
+        },
+        {
+            icon: < FaClock className='text-pink-600 text-xl'/>,
+            title: 'Working Hours',
+            content: (
+                <p className='text-gray-600'>
+                  Appointments: <a href="#" className='hover:text-pink-600'>Dindabeautycare@gmail.com</a>    
                 </p>
             )      
         }
