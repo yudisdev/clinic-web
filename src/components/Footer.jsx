ed3bde6ede5d791ca5e4cfce61a584de9ede1420
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-11-20 21:39:12 +0800

    update footer about sociallinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,20 @@
 import React from "react";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 
-const Footer = () => 
+const Footer = () => {
+    const sociallinks = [
+        {icon: <FaFacebookF classname="text-lg"/>, label: "Facebook", href: "#"},
+        {icon: <FaInstagram classname="text-lg"/>, label: "Facebook", href: "#"},
+        {icon: <FaWhatsapp classname="text-lg"/>, label: "Facebook", href: "#"},
+    ]
+
+    const quiclinks = []
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer;
